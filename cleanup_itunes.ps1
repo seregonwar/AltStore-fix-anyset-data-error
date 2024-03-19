4af06f1a22e07b5d5d5c36b3d64dd5adf4c7e244
Seregon <109359355+seregonwar@users.noreply.github.com>
2024-03-19 18:51:59 +0100

    cleanup_itunes.ps1
    Add the first script

---
+++ cleanup_itunes.ps1
@@ -0,0 +1,18 @@
+# Defining the path of the directory to clean
+$directoryPath = "C:\ProgramData\Apple Computer\iTunes\adi"
+
+# If the directory exists, delete all files in it
+if (Test-Path $directoryPath) {
+    Remove-Item -Path "$directoryPath\*" -Force -Recurse
+} else {
+    Write-Host "La directory $directoryPath non esiste."
+}
+
+# Restart iTunes
+$itunesProcess = Get-Process -Name iTunes -ErrorAction SilentlyContinue
+if ($itunesProcess) {
+    Stop-Process -Name iTunes -Force
+    Start-Process "C:\Program Files\iTunes\iTunes.exe"
+} else {
+    Write-Host "iTunes is not running."
+}
